da36836da5752db387c7a8c2148f147d30c2e8eb
iranhonorato <antonioihsj@al.insper.edu.br>
2025-06-17 14:15:25 -0300

    Inicializando flask

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS 
+import sqlite3 
+
+
+app = Flask(__name__)
+CORS(app) 
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+if __name__ == '__main__':
+    app.run(debug=True)
