0cc370e2e560c322add078187753be0557a32182
iranhonorato <antonioihsj@al.insper.edu.br>
2025-06-17 14:35:03 -0300

    Pega tarefas

--- app.py
+++ app.py
@@ -6,9 +6,31 @@ import sqlite3
 app = Flask(__name__)
 CORS(app) 
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+
+
+def conectar_db():
+    return sqlite3.connect("db_gerenciador_tarefas.db")
+
+
+
+@app.route("/tarefas", methods=["GET"])
+def get_tarefas():
+
+    conn = conectar_db()
+    if conn is None: 
+        return jsonify({"error": "Erro ao conectar com o banco de dados"}), 500 
+    
+    cursor = conn.cursor()
+    sql_command = "SELECT * FROM tarefas"
+    cursor.execute(sql_command)
+    
+    resultados = cursor.fetchall()
+    if not resultados:
+        return jsonify([]), 404
+    
+    tarefas = [{"id": tarefa[0], "titulo": tarefa[1], "descricao": tarefa[2]} for tarefa in resultados]
+    return jsonify(tarefas), 200 
+
 
 if __name__ == '__main__':
     app.run(debug=True)
